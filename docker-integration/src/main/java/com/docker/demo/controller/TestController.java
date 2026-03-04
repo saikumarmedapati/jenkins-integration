e8a3b61cf194b44f898d9d516c914667a3715323
saikumarmedapati <medapatisaikumar1605@gmail.com>
2026-03-04 15:12:26 +0530

    hook-success

--- docker-integration/src/main/java/com/docker/demo/controller/TestController.java
+++ docker-integration/src/main/java/com/docker/demo/controller/TestController.java
@@ -11,6 +11,6 @@ public class TestController {
 
     @GetMapping
     public ResponseEntity<String> sampleResponse() {
-        return ResponseEntity.ok("Hai SconexSoft members. Good AfterNoon- Implemented Git hook");
+        return ResponseEntity.ok("HI SCONEXSOFT I SUCCESSFULLY IMPLEMENTED THE GITHUB WEBHOOK");
     }
 }
